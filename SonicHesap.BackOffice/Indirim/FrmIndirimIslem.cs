44a1c69d54ccb749251bbcf02ad783e92a520205
Tuncay Çakıroğlu <Tuncay.cakiroglu@icloud.com>
2024-02-22 01:36:41 +0300

    İndirim Sihirbazı Tasarımı Kodlaması Yapıldı.

---
+++ SonicHesap.BackOffice/Indirim/FrmIndirimIslem.cs
@@ -0,0 +1,104 @@
+﻿using DevExpress.XtraEditors;
+using SonicHesap.BackOffice.Stok;
+using SonicHesap.Entities.Context;
+using SonicHesap.Entities.Data_Access;
+using SonicHesap.Entities.Tables;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SonicHesap.BackOffice.Indirim
+{
+    public partial class FrmIndirimIslem : DevExpress.XtraEditors.XtraForm
+    {
+        SonicHesapContext context = new SonicHesapContext();
+        IndirimDAL IndirimDal = new IndirimDAL();
+        public FrmIndirimIslem()
+        {
+            InitializeComponent();
+            gridContIndirimshr.DataSource = context.Indirimler.Local.ToBindingList();
+        }
+
+        private Entities.Tables.Indirim StokEkle(Entities.Tables.Stok entity)
+        {
+            Entities.Tables.Indirim _entity = new Entities.Tables.Indirim();
+            _entity.StokKodu = entity.StokKodu;
+            _entity.Barkod = entity.Barkod;
+            _entity.StokAdi = entity.StokAdi;
+            return _entity;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            FrmStokSec form = new FrmStokSec(true);
+            form.ShowDialog();
+            if (form.secildi)
+            {
+                foreach (var itemStok in form.secilen)
+                {
+                    Entities.Tables.Indirim _entity = new Entities.Tables.Indirim();
+                    _entity = StokEkle(itemStok);
+                    var count = context.Indirimler.Count(c => c.StokKodu == itemStok.StokKodu);
+                    if (count != 0)
+                    {
+                        if (MessageBox.Show("Seçili Olan Stoğa Daha Önceden Eklenmiş Bir İndirim Bulunmaktadır.Var Olan İndirimi Güncellemek İstermisiniz?", "Uyarı!", MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            var secilenId = context.Indirimler.SingleOrDefault(c => c.StokKodu == itemStok.StokKodu);
+                            _entity.Id = secilenId.Id;
+                            IndirimDal.AddOrUpdate(context, _entity);
+                            MessageBox.Show("Stok İndirimi Güncellendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    else
+                    {
+                        IndirimDal.AddOrUpdate(context, _entity);
+                    }
+                }
+            }
+        }
+
+        private void wizardControl1_FinishClick(object sender, CancelEventArgs e)
+        {
+            foreach (var itemIndirim in context.Indirimler.Local.ToList())
+            {
+                if (btnSuresiz.Checked)
+                {
+                    itemIndirim.IndirimTuru = "Süresiz";
+                }
+                else
+                {
+                    itemIndirim.BaslangicTarihi = dateBaslangic.DateTime;
+                    itemIndirim.BitisTarihi = dateBitis.DateTime;
+                    itemIndirim.IndirimTuru = "Belirli Tarihler Arasında";
+                }
+                itemIndirim.BaslangicTarihi = dateBaslangic.DateTime;
+                itemIndirim.BitisTarihi = dateBitis.DateTime;
+                itemIndirim.Durumu = true;
+            }
+            IndirimDal.Save(context);
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Seçili Olan Veriyi Listeden Çıkarmak İstediğinize Eminmisiniz?","Uyarı!",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+            {
+                var secilenStokKodu = gridIndirim.GetFocusedRowCellValue(colStokKodu).ToString();
+                var secilen = IndirimDal.GetByFilter(context, c => c.StokKodu == secilenStokKodu);
+                context.Entry(secilen).State = EntityState.Detached;
+                MessageBox.Show("Stok Listeden Çıkarıldı!","Bilgi",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                return;
+            }
+           
+        }
+    }
+}
---
+++ SonicHesap.BackOffice/Indirim/FrmIndirimislemleri.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace SonicHesap.BackOffice.Indirim
+{
+    partial class FrmIndirimislemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmIndirimislemleri));
+            this.grpMenu = new DevExpress.XtraEditors.GroupControl();
+            this.btnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.btnAra = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDuzenle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.lblBaslik = new DevExpress.XtraEditors.LabelControl();
+            this.gridContIndirim = new DevExpress.XtraGrid.GridControl();
+            this.gridIndirim = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDurumu = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStokKodu = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBarkod = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStokAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colIndirimTuru = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBaslangicTarihi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBitisTarihi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colIndirimOrani = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAciklama = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).BeginInit();
+            this.grpMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridContIndirim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridIndirim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpMenu
+            // 
+            this.grpMenu.Controls.Add(this.btnSil);
+            this.grpMenu.Controls.Add(this.btnKapat);
+            this.grpMenu.Controls.Add(this.btnAra);
+            this.grpMenu.Controls.Add(this.btnGuncelle);
+            this.grpMenu.Controls.Add(this.btnDuzenle);
+            this.grpMenu.Controls.Add(this.btnEkle);
+            this.grpMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grpMenu.Location = new System.Drawing.Point(0, 641);
+            this.grpMenu.Name = "grpMenu";
+            this.grpMenu.Size = new System.Drawing.Size(1338, 83);
+            this.grpMenu.TabIndex = 6;
+            this.grpMenu.Text = "Menü";
+            // 
+            // btnSil
+            // 
+            this.btnSil.ImageOptions.ImageIndex = 9;
+            this.btnSil.ImageOptions.ImageList = this.ımageList1;
+            this.btnSil.Location = new System.Drawing.Point(225, 28);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(111, 53);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "add.png");
+            this.ımageList1.Images.SetKeyName(1, "clock_run.png");
+            this.ımageList1.Images.SetKeyName(2, "delete.png");
+            this.ımageList1.Images.SetKeyName(3, "düzenle.png");
+            this.ımageList1.Images.SetKeyName(4, "ekle.png");
+            this.ımageList1.Images.SetKeyName(5, "folder_out.png");
+            this.ımageList1.Images.SetKeyName(6, "pencil.png");
+            this.ımageList1.Images.SetKeyName(7, "remotecontrol2.png");
+            this.ımageList1.Images.SetKeyName(8, "replace2.png");
+            this.ımageList1.Images.SetKeyName(9, "sil.png");
+            this.ımageList1.Images.SetKeyName(10, "sort_descending.png");
+            this.ımageList1.Images.SetKeyName(11, "stopwatch_run.png");
+            this.ımageList1.Images.SetKeyName(12, "view.png");
+            this.ımageList1.Images.SetKeyName(13, "refresh.png");
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnKapat.ImageOptions.ImageIndex = 5;
+            this.btnKapat.ImageOptions.ImageList = this.ımageList1;
+            this.btnKapat.Location = new System.Drawing.Point(1242, 28);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(94, 53);
+            this.btnKapat.TabIndex = 0;
+            this.btnKapat.Text = "Kapat";
+            // 
+            // btnAra
+            // 
+            this.btnAra.ImageOptions.ImageIndex = 12;
+            this.btnAra.ImageOptions.ImageList = this.ımageList1;
+            this.btnAra.Location = new System.Drawing.Point(449, 28);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(111, 53);
+            this.btnAra.TabIndex = 0;
+            this.btnAra.Text = "Ara";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageOptions.ImageIndex = 13;
+            this.btnGuncelle.ImageOptions.ImageList = this.ımageList1;
+            this.btnGuncelle.Location = new System.Drawing.Point(337, 28);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(111, 53);
+            this.btnGuncelle.TabIndex = 0;
+            this.btnGuncelle.Text = "Güncelle";
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDuzenle.ImageOptions.ImageIndex = 3;
+            this.btnDuzenle.ImageOptions.ImageList = this.ımageList1;
+            this.btnDuzenle.Location = new System.Drawing.Point(113, 28);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(111, 53);
+            this.btnDuzenle.TabIndex = 0;
+            this.btnDuzenle.Text = "Düzenle";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnEkle.ImageOptions.ImageIndex = 4;
+            this.btnEkle.ImageOptions.ImageList = this.ımageList1;
+            this.btnEkle.Location = new System.Drawing.Point(2, 28);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(111, 53);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Appearance.Options.UseFont = true;
+            this.lblBaslik.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lblBaslik.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.lblBaslik.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblBaslik.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.lblBaslik.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBaslik.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("lblBaslik.ImageOptions.Image")));
+            this.lblBaslik.Location = new System.Drawing.Point(0, 0);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(1338, 66);
+            this.lblBaslik.TabIndex = 4;
+            this.lblBaslik.Text = "İndirimler";
+            // 
+            // gridContIndirim
+            // 
+            this.gridContIndirim.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridContIndirim.Location = new System.Drawing.Point(0, 66);
+            this.gridContIndirim.MainView = this.gridIndirim;
+            this.gridContIndirim.Name = "gridContIndirim";
+            this.gridContIndirim.Size = new System.Drawing.Size(1338, 575);
+            this.gridContIndirim.TabIndex = 5;
+            this.gridContIndirim.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridIndirim});
+            // 
+            // gridIndirim
+            // 
+            this.gridIndirim.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colDurumu,
+            this.colStokKodu,
+            this.colBarkod,
+            this.colStokAdi,
+            this.colIndirimTuru,
+            this.colBaslangicTarihi,
+            this.colBitisTarihi,
+            this.colIndirimOrani,
+            this.colAciklama});
+            this.gridIndirim.GridControl = this.gridContIndirim;
+            this.gridIndirim.Name = "gridIndirim";
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.MinWidth = 25;
+            this.colId.Name = "colId";
+            this.colId.OptionsColumn.AllowEdit = false;
+            this.colId.OptionsColumn.ShowInCustomizationForm = false;
+            this.colId.Width = 94;
+            // 
+            // colDurumu
+            // 
+            this.colDurumu.FieldName = "Durumu";
+            this.colDurumu.MinWidth = 25;
+            this.colDurumu.Name = "colDurumu";
+            this.colDurumu.OptionsColumn.AllowEdit = false;
+            this.colDurumu.OptionsColumn.ShowInCustomizationForm = false;
+            this.colDurumu.Visible = true;
+            this.colDurumu.VisibleIndex = 0;
+            this.colDurumu.Width = 94;
+            // 
+            // colStokKodu
+            // 
+            this.colStokKodu.FieldName = "StokKodu";
+            this.colStokKodu.MinWidth = 25;
+            this.colStokKodu.Name = "colStokKodu";
+            this.colStokKodu.OptionsColumn.AllowEdit = false;
+            this.colStokKodu.OptionsColumn.ShowInCustomizationForm = false;
+            this.colStokKodu.Visible = true;
+            this.colStokKodu.VisibleIndex = 1;
+            this.colStokKodu.Width = 94;
+            // 
+            // colBarkod
+            // 
+            this.colBarkod.FieldName = "Barkod";
+            this.colBarkod.MinWidth = 25;
+            this.colBarkod.Name = "colBarkod";
+            this.colBarkod.OptionsColumn.AllowEdit = false;
+            this.colBarkod.OptionsColumn.ShowInCustomizationForm = false;
+            this.colBarkod.Visible = true;
+            this.colBarkod.VisibleIndex = 2;
+            this.colBarkod.Width = 94;
+            // 
+            // colStokAdi
+            // 
+            this.colStokAdi.FieldName = "StokAdi";
+            this.colStokAdi.MinWidth = 25;
+            this.colStokAdi.Name = "colStokAdi";
+            this.colStokAdi.OptionsColumn.AllowEdit = false;
+            this.colStokAdi.OptionsColumn.ShowInCustomizationForm = false;
+            this.colStokAdi.Visible = true;
+            this.colStokAdi.VisibleIndex = 3;
+            this.colStokAdi.Width = 94;
+            // 
+            // colIndirimTuru
+            // 
+            this.colIndirimTuru.FieldName = "IndirimTuru";
+            this.colIndirimTuru.MinWidth = 25;
+            this.colIndirimTuru.Name = "colIndirimTuru";
+            this.colIndirimTuru.OptionsColumn.AllowEdit = false;
+            this.colIndirimTuru.OptionsColumn.ShowInCustomizationForm = false;
+            this.colIndirimTuru.Visible = true;
+            this.colIndirimTuru.VisibleIndex = 4;
+            this.colIndirimTuru.Width = 94;
+            // 
+            // colBaslangicTarihi
+            // 
+            this.colBaslangicTarihi.FieldName = "BaslangicTarihi";
+            this.colBaslangicTarihi.MinWidth = 25;
+            this.colBaslangicTarihi.Name = "colBaslangicTarihi";
+            this.colBaslangicTarihi.OptionsColumn.AllowEdit = false;
+            this.colBaslangicTarihi.OptionsColumn.ShowInCustomizationForm = false;
+            this.colBaslangicTarihi.Visible = true;
+            this.colBaslangicTarihi.VisibleIndex = 5;
+            this.colBaslangicTarihi.Width = 94;
+            // 
+            // colBitisTarihi
+            // 
+            this.colBitisTarihi.FieldName = "BitisTarihi";
+            this.colBitisTarihi.MinWidth = 25;
+            this.colBitisTarihi.Name = "colBitisTarihi";
+            this.colBitisTarihi.OptionsColumn.AllowEdit = false;
+            this.colBitisTarihi.OptionsColumn.ShowInCustomizationForm = false;
+            this.colBitisTarihi.Visible = true;
+            this.colBitisTarihi.VisibleIndex = 6;
+            this.colBitisTarihi.Width = 94;
+            // 
+            // colIndirimOrani
+            // 
+            this.colIndirimOrani.FieldName = "IndirimOrani";
+            this.colIndirimOrani.MinWidth = 25;
+            this.colIndirimOrani.Name = "colIndirimOrani";
+            this.colIndirimOrani.OptionsColumn.AllowEdit = false;
+            this.colIndirimOrani.OptionsColumn.ShowInCustomizationForm = false;
+            this.colIndirimOrani.Visible = true;
+            this.colIndirimOrani.VisibleIndex = 7;
+            this.colIndirimOrani.Width = 94;
+            // 
+            // colAciklama
+            // 
+            this.colAciklama.FieldName = "Aciklama";
+            this.colAciklama.MinWidth = 25;
+            this.colAciklama.Name = "colAciklama";
+            this.colAciklama.OptionsColumn.AllowEdit = false;
+            this.colAciklama.OptionsColumn.ShowInCustomizationForm = false;
+            this.colAciklama.Visible = true;
+            this.colAciklama.VisibleIndex = 8;
+            this.colAciklama.Width = 94;
+            // 
+            // FrmIndirimislemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1338, 724);
+            this.Controls.Add(this.gridContIndirim);
+            this.Controls.Add(this.grpMenu);
+            this.Controls.Add(this.lblBaslik);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmIndirimislemleri";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İndirimler";
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).EndInit();
+            this.grpMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridContIndirim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridIndirim)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl grpMenu;
+        private DevExpress.XtraEditors.SimpleButton btnSil;
+        private System.Windows.Forms.ImageList ımageList1;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+        private DevExpress.XtraEditors.SimpleButton btnAra;
+        private DevExpress.XtraEditors.SimpleButton btnGuncelle;
+        private DevExpress.XtraEditors.SimpleButton btnDuzenle;
+        private DevExpress.XtraEditors.SimpleButton btnEkle;
+        private DevExpress.XtraEditors.LabelControl lblBaslik;
+        private DevExpress.XtraGrid.GridControl gridContIndirim;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridIndirim;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colDurumu;
+        private DevExpress.XtraGrid.Columns.GridColumn colStokKodu;
+        private DevExpress.XtraGrid.Columns.GridColumn colBarkod;
+        private DevExpress.XtraGrid.Columns.GridColumn colStokAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colIndirimTuru;
+        private DevExpress.XtraGrid.Columns.GridColumn colBaslangicTarihi;
+        private DevExpress.XtraGrid.Columns.GridColumn colBitisTarihi;
+        private DevExpress.XtraGrid.Columns.GridColumn colIndirimOrani;
+        private DevExpress.XtraGrid.Columns.GridColumn colAciklama;
+    }
+}
