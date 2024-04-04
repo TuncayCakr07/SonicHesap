4ac703c4ce0008eea882d0869b8c98cdd71ec2f3
Tuncay Çakıroğlu <100436327+TuncayCakr07@users.noreply.github.com>
2024-04-05 02:24:35 +0300

    Kullanıcı Formu Tasarlandı

---
+++ SonicHesap.Admin/FrmKullanicilar.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace SonicHesap.Admin
+{
+    partial class FrmKullanicilar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKullanicilar));
+            this.lblBaslik = new DevExpress.XtraEditors.LabelControl();
+            this.grpMenu = new DevExpress.XtraEditors.GroupControl();
+            this.btnAra = new DevExpress.XtraEditors.SimpleButton();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDuzenle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.kullaniciBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.colKullaniciAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSoyadi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colGorevi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colKayitTarihi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSonGirisTarihi = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).BeginInit();
+            this.grpMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kullaniciBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.Appearance.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Appearance.Options.UseFont = true;
+            this.lblBaslik.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lblBaslik.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.lblBaslik.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblBaslik.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.lblBaslik.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBaslik.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("lblBaslik.ImageOptions.Image")));
+            this.lblBaslik.Location = new System.Drawing.Point(0, 0);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(1256, 66);
+            this.lblBaslik.TabIndex = 6;
+            this.lblBaslik.Text = "Kullanıcılar";
+            // 
+            // grpMenu
+            // 
+            this.grpMenu.Controls.Add(this.btnAra);
+            this.grpMenu.Controls.Add(this.btnKapat);
+            this.grpMenu.Controls.Add(this.btnGuncelle);
+            this.grpMenu.Controls.Add(this.btnSil);
+            this.grpMenu.Controls.Add(this.btnDuzenle);
+            this.grpMenu.Controls.Add(this.btnEkle);
+            this.grpMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grpMenu.Location = new System.Drawing.Point(0, 575);
+            this.grpMenu.Name = "grpMenu";
+            this.grpMenu.Size = new System.Drawing.Size(1256, 83);
+            this.grpMenu.TabIndex = 7;
+            this.grpMenu.Text = "Menü";
+            // 
+            // btnAra
+            // 
+            this.btnAra.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAra.ImageOptions.ImageIndex = 2;
+            this.btnAra.ImageOptions.ImageList = this.ımageList1;
+            this.btnAra.Location = new System.Drawing.Point(446, 28);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(111, 53);
+            this.btnAra.TabIndex = 2;
+            this.btnAra.Text = "Ara";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "folder_out.png");
+            this.ımageList1.Images.SetKeyName(1, "refresh.png");
+            this.ımageList1.Images.SetKeyName(2, "view.png");
+            this.ımageList1.Images.SetKeyName(3, "id_card.png");
+            this.ımageList1.Images.SetKeyName(4, "id_card_add.png");
+            this.ımageList1.Images.SetKeyName(5, "id_card_delete.png");
+            this.ımageList1.Images.SetKeyName(6, "id_card_edit.png");
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnKapat.ImageOptions.ImageIndex = 0;
+            this.btnKapat.ImageOptions.ImageList = this.ımageList1;
+            this.btnKapat.Location = new System.Drawing.Point(1160, 28);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(94, 53);
+            this.btnKapat.TabIndex = 0;
+            this.btnKapat.Text = "Kapat";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnGuncelle.ImageOptions.ImageIndex = 1;
+            this.btnGuncelle.ImageOptions.ImageList = this.ımageList1;
+            this.btnGuncelle.Location = new System.Drawing.Point(335, 28);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(111, 53);
+            this.btnGuncelle.TabIndex = 0;
+            this.btnGuncelle.Text = "Güncelle";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSil.ImageOptions.ImageIndex = 5;
+            this.btnSil.ImageOptions.ImageList = this.ımageList1;
+            this.btnSil.Location = new System.Drawing.Point(224, 28);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(111, 53);
+            this.btnSil.TabIndex = 0;
+            this.btnSil.Text = "Sil";
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDuzenle.ImageOptions.ImageIndex = 6;
+            this.btnDuzenle.ImageOptions.ImageList = this.ımageList1;
+            this.btnDuzenle.Location = new System.Drawing.Point(113, 28);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(111, 53);
+            this.btnDuzenle.TabIndex = 0;
+            this.btnDuzenle.Text = "Düzenle";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnEkle.ImageOptions.ImageIndex = 4;
+            this.btnEkle.ImageOptions.ImageList = this.ımageList1;
+            this.btnEkle.Location = new System.Drawing.Point(2, 28);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(111, 53);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.kullaniciBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 66);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1256, 509);
+            this.gridControl1.TabIndex = 8;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colKullaniciAdi,
+            this.colAdi,
+            this.colSoyadi,
+            this.colGorevi,
+            this.colKayitTarihi,
+            this.colSonGirisTarihi});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // kullaniciBindingSource
+            // 
+            this.kullaniciBindingSource.DataSource = typeof(SonicHesap.Entities.Tables.Kullanici);
+            // 
+            // colKullaniciAdi
+            // 
+            this.colKullaniciAdi.Caption = "Kullanıcı Adı";
+            this.colKullaniciAdi.FieldName = "KullaniciAdi";
+            this.colKullaniciAdi.MinWidth = 25;
+            this.colKullaniciAdi.Name = "colKullaniciAdi";
+            this.colKullaniciAdi.OptionsColumn.AllowEdit = false;
+            this.colKullaniciAdi.Visible = true;
+            this.colKullaniciAdi.VisibleIndex = 0;
+            this.colKullaniciAdi.Width = 94;
+            // 
+            // colAdi
+            // 
+            this.colAdi.Caption = "Adı";
+            this.colAdi.FieldName = "Adi";
+            this.colAdi.MinWidth = 25;
+            this.colAdi.Name = "colAdi";
+            this.colAdi.OptionsColumn.AllowEdit = false;
+            this.colAdi.Visible = true;
+            this.colAdi.VisibleIndex = 1;
+            this.colAdi.Width = 94;
+            // 
+            // colSoyadi
+            // 
+            this.colSoyadi.Caption = "Soyadı";
+            this.colSoyadi.FieldName = "Soyadi";
+            this.colSoyadi.MinWidth = 25;
+            this.colSoyadi.Name = "colSoyadi";
+            this.colSoyadi.OptionsColumn.AllowEdit = false;
+            this.colSoyadi.Visible = true;
+            this.colSoyadi.VisibleIndex = 2;
+            this.colSoyadi.Width = 94;
+            // 
+            // colGorevi
+            // 
+            this.colGorevi.Caption = "Görevi";
+            this.colGorevi.FieldName = "Gorevi";
+            this.colGorevi.MinWidth = 25;
+            this.colGorevi.Name = "colGorevi";
+            this.colGorevi.OptionsColumn.AllowEdit = false;
+            this.colGorevi.Visible = true;
+            this.colGorevi.VisibleIndex = 3;
+            this.colGorevi.Width = 94;
+            // 
+            // colKayitTarihi
+            // 
+            this.colKayitTarihi.Caption = "Kayıt Tarihi";
+            this.colKayitTarihi.FieldName = "KayitTarihi";
+            this.colKayitTarihi.MinWidth = 25;
+            this.colKayitTarihi.Name = "colKayitTarihi";
+            this.colKayitTarihi.OptionsColumn.AllowEdit = false;
+            this.colKayitTarihi.Visible = true;
+            this.colKayitTarihi.VisibleIndex = 4;
+            this.colKayitTarihi.Width = 94;
+            // 
+            // colSonGirisTarihi
+            // 
+            this.colSonGirisTarihi.Caption = "Son Giriş Tarihi";
+            this.colSonGirisTarihi.FieldName = "SonGirisTarihi";
+            this.colSonGirisTarihi.MinWidth = 25;
+            this.colSonGirisTarihi.Name = "colSonGirisTarihi";
+            this.colSonGirisTarihi.OptionsColumn.AllowEdit = false;
+            this.colSonGirisTarihi.Visible = true;
+            this.colSonGirisTarihi.VisibleIndex = 5;
+            this.colSonGirisTarihi.Width = 94;
+            // 
+            // FrmKullanicilar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 658);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.grpMenu);
+            this.Controls.Add(this.lblBaslik);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmKullanicilar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kullanıcılar";
+            this.Load += new System.EventHandler(this.FrmKullanicilar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).EndInit();
+            this.grpMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kullaniciBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lblBaslik;
+        private DevExpress.XtraEditors.GroupControl grpMenu;
+        private System.Windows.Forms.ImageList ımageList1;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+        private DevExpress.XtraEditors.SimpleButton btnSil;
+        private DevExpress.XtraEditors.SimpleButton btnDuzenle;
+        private DevExpress.XtraEditors.SimpleButton btnEkle;
+        private DevExpress.XtraEditors.SimpleButton btnAra;
+        private DevExpress.XtraEditors.SimpleButton btnGuncelle;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource kullaniciBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colKullaniciAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colSoyadi;
+        private DevExpress.XtraGrid.Columns.GridColumn colGorevi;
+        private DevExpress.XtraGrid.Columns.GridColumn colKayitTarihi;
+        private DevExpress.XtraGrid.Columns.GridColumn colSonGirisTarihi;
+    }
+}
+
---
+++ SonicHesap.Admin/FrmKullanicilar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SonicHesap.Admin
+{
+    public partial class FrmKullanicilar : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmKullanicilar()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmKullanicilar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
